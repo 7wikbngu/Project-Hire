0de779db4571219ea9846a1cdb369ba9261a96c0
Sathwik <sathwikdec@gmail.com>
2019-01-08 15:46:59 -0500

    added position numbers

--- scripts/app.js
+++ scripts/app.js
@@ -14,15 +14,24 @@ $(function() {
     function createJobs(data){
         var catAll = [];
         var id = [];
+        var count = [];
 
         for(var i = 0; i < data.length; i++){
             catAll[i] = data[i].hiringOrganization.department.name;
         }
         const category = [...new Set(catAll)];
-
+        for(var i = 0; i < category.length; i++){
+            count[i] = 0;
+            for(var j = 0; j < catAll.length; j++){
+                if(catAll[j] === category[i]){
+                    count[i]++;
+                }
+            }
+        }
+        console.log(count);
         for(var i = 0; i < category.length; i++){
             id[i] = category[i].replace(/\s+/g, '')
-            var categoryHTML = '<div id="'+id[i].substring(0,2)+'" class="w-75 shadow px-6 py-5 mb-4 bg-white rounded"><h4 class="ribbon">'+category[i]+'</h1><div class="accordion" id="accordionExample"></div></div>';
+            var categoryHTML = '<div id="'+id[i].substring(0,2)+'" class="w-75 shadow px-6 py-5 mb-4 bg-white rounded"><h4 class="ribbon">'+category[i]+'</h4><p class="float-right">'+count[i]+' position/s</p><div class="accordion" id="accordionExample"></div></div>';
             $('#category').append(categoryHTML);
         }
         console.log(id);
@@ -30,8 +39,9 @@ $(function() {
             var title = data[i].title;
             var location = data[i].jobLocation.address.addressLocality+', '+data[i].jobLocation.address.addressRegion +', '+ data[i].jobLocation.address.addressCountry;
             var department = "#" + data[i].hiringOrganization.department.name.substring(0,2);
+            var description = data[i].description;
 
-            var jobHTML = '<div class="card mb-3 shadow"><div class="card-header bg-white" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div id="collapse'+i+'" class="collapse" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt</div></div></div>';
+            var jobHTML = '<div class="card mb-3 shadow-none"><div class="card-header" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div id="collapse'+i+'" class="collapse" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body text-muted">'+description+'<button class="btn btn-outline-primary mt-4">Apply Now</button></div></div></div><hr>';
             $(department).append(jobHTML);
         }
     }
