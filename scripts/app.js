cfc14f2fa72d6a8ed9c33a7e42557b8eb75e5173
Sathwik <sathwikdec@gmail.com>
2019-01-08 15:07:17 -0500

    intial commit changes

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,7 @@ $(function() {
     function createJobs(data){
         var catAll = [];
         var id = [];
+
         for(var i = 0; i < data.length; i++){
             catAll[i] = data[i].hiringOrganization.department.name;
         }
@@ -24,16 +25,15 @@ $(function() {
             var categoryHTML = '<div id="'+id[i].substring(0,2)+'" class="w-75 shadow px-6 py-5 mb-4 bg-white rounded"><h4 class="ribbon">'+category[i]+'</h1><div class="accordion" id="accordionExample"></div></div>';
             $('#category').append(categoryHTML);
         }
-
+        console.log(id);
         for(var i = 0; i < data.length; i++){
             var title = data[i].title;
             var location = data[i].jobLocation.address.addressLocality+', '+data[i].jobLocation.address.addressRegion +', '+ data[i].jobLocation.address.addressCountry;
             var department = "#" + data[i].hiringOrganization.department.name.substring(0,2);
 
-            var jobHTML = '<div class="card mb-4"><div class="card-header" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'"><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div id="collapse'+i+'" class="collapse show" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt</div></div></div>';
+            var jobHTML = '<div class="card mb-3 shadow"><div class="card-header bg-white" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div id="collapse'+i+'" class="collapse" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt</div></div></div>';
             $(department).append(jobHTML);
         }
-        $("#accordion").accordion({collapsible : true, active : false});
     }
 
 });
