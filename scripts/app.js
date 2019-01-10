4af8163518f6b3e07f4dc743cf045e78aac7eaed
Sathwik <sathwikdec@gmail.com>
2019-01-10 12:17:11 -0500

    changed app.js and updated app2.js

--- scripts/app.js
+++ scripts/app.js
@@ -11,10 +11,12 @@ $(function() {
         }
     });
 
+
     function createJobs(data){
         var catAll = [];
         var id = [];
         var count = [];
+        var categoryHTML = "";
 
         for(var i = 0; i < data.length; i++){
             catAll[i] = data[i].hiringOrganization.department.name;
@@ -28,22 +30,40 @@ $(function() {
                 }
             }
         }
+
         console.log(count);
         for(var i = 0; i < category.length; i++){
             id[i] = category[i].replace(/\s+/g, '')
-            var categoryHTML = '<div id="'+id[i].substring(0,2)+'" class="w-75 shadow px-6 py-5 mb-4 bg-white rounded text-middle"><h4 class="ribbon">'+category[i]+'</h4><p class="number-of-jobs float-right mt-3">'+count[i]+' position(s)</p><div class="accordion" id="accordionExample"></div></div>';
-            $('#category').append(categoryHTML);
+            categoryHTML += '<div id="'+id[i].substring(0,2)+'" class="w-75 shadow px-6 py-5 mb-4 bg-white rounded text-middle"><h4 class="ribbon">'+category[i]+'</h4><p class="number-of-jobs float-right mt-3">'+count[i]+' position(s)</p><div class="accordion" id="accordionExample">';
         }
+        
         console.log(id);
         for(var i = 0; i < data.length; i++){
             var title = data[i].title;
             var location = data[i].jobLocation.address.addressLocality+', '+data[i].jobLocation.address.addressRegion +', '+ data[i].jobLocation.address.addressCountry;
-            var department = "#" + data[i].hiringOrganization.department.name.substring(0,2);
+            var departmentID = "#" + data[i].hiringOrganization.department.name.substring(0,2);
             var description = data[i].description;
-
-            var jobHTML = '<div class="card mb-3 left-text"><div class="card-header bg-white d-flex flex-row justify-content-between" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><div><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div class="align-self-center text-blue"><i class="fas fa-chevron-down"></i><i class="fas fa-chevron-up"></i></div></div><div id="collapse'+i+'" class="collapse" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body text-muted">'+description+'<button class="btn btn-outline-primary mt-4">Apply Now</button></div></div></div>';
-            $(department).append(jobHTML);
+            if(i === 0){
+                var jobHTML = '<div class="card mb-3 left-text"><div class="card-header bg-white d-flex flex-row justify-content-between" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><div><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div class="align-self-center text-blue"><i class="fas fa-chevron-down"></i><i class="fas fa-chevron-up"></i></div></div><div id="collapse'+i+'" class="collapse show" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body text-muted">'+description+'<button class="btn btn-outline-primary mt-4">Apply Now</button></div></div></div>';
+            }else {
+                var jobHTML = '<div class="card mb-3 left-text"><div class="card-header bg-white d-flex flex-row justify-content-between" id="heading'+i+'" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'"><div><h5 class="mb-0 text-blue">'+title+'</h5><p class="text-muted">'+location+'</p></div><div class="align-self-center text-blue"><i class="fas fa-chevron-down"></i><i class="fas fa-chevron-up"></i></div></div><div id="collapse'+i+'" class="collapse" aria-labelledby="heading'+i+'" data-parent="#accordionExample"><div class="card-body text-muted">'+description+'<button class="btn btn-outline-primary mt-4">Apply Now</button></div></div></div>';
+            }
+            $(departmentID).append(jobHTML);
         }
-    }
 
+        for(var i = 0; i < category.length; i++){
+            var departmentID = "#" + data[i].hiringOrganization.department.name.substring(0,2);
+            $(departmentID).append('</div></div>');
+        }
+
+
+        // (function clickEvent(){
+        //     $('.card-header').click( (e) => {
+        //         var element = e.currentTarget;
+        //         if()
+        //         $(".collapse.show").removeClass("show");
+        //     });
+        // }())
+    }
+    
 });
---
+++ scripts/app2.js
@@ -0,0 +1,57 @@
+$(function() {
+    $.ajax({
+        url: "https://hire.withgoogle.com/v2/api/t/openlogixcom/public/jobs", 
+        dataType: "json", 
+        success: function(result){
+            console.log(result);
+            createJobs(result);
+        },
+        error: function(err){
+            console.log("Error:",err);
+        }
+    });
+});
+
+function createJobs(json){
+
+    let categories = [];
+    let departmentsObj = {};
+    let categoryHTML = '';
+
+    // Get Categories
+    for(var i = 0; i < json.length; i++){
+        let department = json[i].hiringOrganization.department.name;
+        
+        // Category not exists 
+        // Create category and add the html
+        if(categories.indexOf(department) === -1){
+            categories.push(department);
+            departmentsObj[department] = [json[i]];
+        } 
+        // Category exists
+        // Push the json object to the associated category
+        else {
+            departmentsObj[department].push(json[i]);
+        }
+    }
+
+    console.log(departmentsObj);
+
+    // APpend the html by category and the job positings
+    categories.forEach(category => {
+        let categoryID = category.substring(0,2);
+        let departmentArr = departmentsObj[category];
+        categoryHTML += '<div id="'+ categoryID+'" class="w-75 shadow px-6 py-5 mb-4 bg-white rounded text-middle"><h4 class="ribbon">'+category+'</h4><p class="number-of-jobs float-right mt-3">'+ departmentArr.length +' position(s)</p>';
+        categoryHTML += '<div class="accordion" id="accordion'+ categoryID +'">';
+
+        departmentArr.forEach((job, i) => {
+            let location =  job.jobLocation.address.addressLocality+', '+ job.jobLocation.address.addressRegion +', '+ job.jobLocation.address.addressCountry;
+            categoryHTML += '<div class="card mb-3 left-text"><div class="card-header bg-white d-flex flex-row justify-content-between" id="' + categoryID + i+'" data-toggle="collapse" data-target="#collapse'+ categoryID + i+'" aria-expanded="false" aria-controls="collapse'+ categoryID + i+'"><div><h5 class="mb-0 text-blue">'+ job.title +'</h5><p class="text-muted">'+ location +'</p></div><div class="align-self-center text-blue"><i class="fas fa-chevron-down"></i><i class="fas fa-chevron-up"></i></div></div><div id="collapse'+ categoryID + i+'" class="collapse" aria-labelledby="'+categoryID + i + '" data-parent="#accordion'+ categoryID +'"><div class="card-body text-muted">'+job.description+'<button class="btn btn-outline-primary mt-4">Apply Now</button></div></div></div>';
+        });
+        categoryHTML += '</div></div>';
+    });
+
+    console.log(departmentsObj);
+    $("#categories").append(categoryHTML);
+
+}
